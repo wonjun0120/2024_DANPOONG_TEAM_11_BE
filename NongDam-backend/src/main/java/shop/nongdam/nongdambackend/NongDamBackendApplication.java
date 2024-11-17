13d2b6a8ebefdeca01a226e93ec2b5b86f33ec7b
dongkyunKim <kyun010713@gmail.com>
2024-11-18 00:40:11 +0900

    chore: 백엔드 프로젝트 초기 설정 및 기본 구조 설정
    - 프로젝트 생성
    - 기본 환경 변수 설정 (애플리케이션 설정, 토큰 만료 시간 등)
    
    Fixes: #1

---
+++ NongDam-backend/src/main/java/shop/nongdam/nongdambackend/NongDamBackendApplication.java
@@ -0,0 +1,13 @@
+package shop.nongdam.nongdambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongDamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NongDamBackendApplication.class, args);
+    }
+
+}
