771d319118f1dbf59e84bde270f44fa681184fcc
dongkyunKim <kyun010713@gmail.com>
2024-11-19 09:06:32 +0900

    역할 필드 추가 및 설명 추가

--- NongDam-backend/src/main/java/shop/nongdam/nongdambackend/member/domain/Role.java
+++ NongDam-backend/src/main/java/shop/nongdam/nongdambackend/member/domain/Role.java
@@ -1,5 +1,18 @@
 package shop.nongdam.nongdambackend.member.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
-    ROLE_ADMIN, ROLE_USER
+    ROLE_ADMIN("관리자"),
+    ROLE_USER("일반 사용자"),
+    ROLE_PRODUCER("농산물 생산자"),
+    ROLE_RESTAURANT("요식업 종사자"),
+    ;
+
+    private final String description;
+
+    Role(String description) {
+        this.description = description;
+    }
 }
